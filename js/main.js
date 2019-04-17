44c424f84efc67c0331b7beec4ecfb771ceed3f6
Dejan <Dejan@DESKTOP-F78QALM>
2019-04-17 20:56:27 +0200

    header complete

--- js/main.js
+++ js/main.js
@@ -1,6 +1,24 @@
 $(document).ready(function(){
    
+    // SIDE BAR TOGGLE ON DESCTOP
     
+    $('.side-bar-toggle').click(function(){
+        $(this).toggleClass('side-bar-toggle-closed');
+        $('.side-bar').toggleClass('side-bar-hide');
+        $('main').toggleClass('main-full-width');
+        //SMALL ARROW DIRECTION
+        if ($('.side-bar-hide').length > 0) {
+            $('.side-bar-toggle').removeClass('fa-angle-left')
+            $('.side-bar-toggle').addClass('fa-angle-right')
+            $('.side-bar-toggle').removeClass('toggle-border-left')
+            $('.side-bar-toggle').addClass('toggle-border-right')
+        } else {
+            $('.side-bar-toggle').removeClass('fa-angle-right')
+            $('.side-bar-toggle').addClass('fa-angle-left')
+            $('.side-bar-toggle').removeClass('toggle-border-right')
+            $('.side-bar-toggle').addClass('toggle-border-left')
+        }
+    })
     
     
     
