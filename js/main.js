6bc0c9761ae1af297cd90e59ec33aaf654e600b2
Dejan <Dejan@DESKTOP-F78QALM>
2019-04-18 01:44:09 +0200

    about and brands sections complete

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-$(document).ready(function(){
-   
+$(document).ready(function () {
+
     // SIDE BAR TOGGLE ON DESCTOP
-    
-    $('.side-bar-toggle').click(function(){
+
+    $('.side-bar-toggle').click(function () {
         $(this).toggleClass('side-bar-toggle-closed');
         $('.side-bar').toggleClass('side-bar-hide');
         $('main').toggleClass('main-full-width');
@@ -19,25 +19,55 @@ $(document).ready(function(){
             $('.side-bar-toggle').addClass('toggle-border-left');
         }
     });
-    
-    
+
+
     // BURGER BUTTON ANIMATION  
-    
-     $('.burger').click(function () {
+
+    $('.burger').click(function () {
         $(this).toggleClass('active burger-move-right');
         $('.mobile-nav').toggleClass('mobile-nav-show');
         $('.body-overlay').toggleClass('body-overlay-show');
     });
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+
+
+    //  OWL BRANDS SLIDER
+
+    if ($('.owl-carousel').length > 0) {
+
+        $('.brands-slider').owlCarousel({
+            loop: true,
+            dots: false,
+            center: true,
+//            autoplay: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                500: {
+                    items: 2
+                },
+                991: {
+                    items: 4
+                },
+                1200: {
+                    items: 5
+                }
+
+            }//responsive end
+        });
+    } // BRANDS SLIDER END
+
+
+
+
+
+
+
+
+
+
+
 }); // DOCUMENT READY END
 
 
