cbae72319f55db4be2ccd5d2ec8ad285bea9e9a5
Dejan <Dejan@DESKTOP-F78QALM>
2019-04-18 22:34:47 +0200

    contact form and validation complete

--- js/main.js
+++ js/main.js
@@ -12,12 +12,12 @@ $('.side-bar-toggle').removeClass('fa-angle-left');
         $('.side-bar-toggle').addClass('fa-angle-right');
         $('.side-bar-toggle').removeClass('toggle-border-left');
         $('.side-bar-toggle').addClass('toggle-border-right');
-        } else {
+} else {
 $('.side-bar-toggle').removeClass('fa-angle-right');
         $('.side-bar-toggle').addClass('fa-angle-left');
         $('.side-bar-toggle').removeClass('toggle-border-right');
         $('.side-bar-toggle').addClass('toggle-border-left');
-        }
+}
 });
         // BURGER BUTTON ANIMATION  
 
@@ -25,7 +25,7 @@ $('.side-bar-toggle').removeClass('fa-angle-right');
 $(this).toggleClass('active burger-move-right');
         $('.mobile-nav').toggleClass('mobile-nav-show');
         $('.body-overlay').toggleClass('body-overlay-show');
-        });
+});
         //  OWL BRANDS SLIDER
 
         if ($('.owl-carousel').length > 0) {
@@ -51,7 +51,7 @@ loop: true,
 
         }//responsive end
 });
-        } // BRANDS SLIDER END
+} // BRANDS SLIDER END
 
 
 
@@ -65,9 +65,9 @@ breakpoints: {small: 0, large: 992}
         const updateLax = () => {
 lax.update(window.scrollY);
         window.requestAnimationFrame(updateLax);
-        };
+};
         window.requestAnimationFrame(updateLax);
-        };
+};
 // OWL TESTIMONIAL SLIDER
 
         if ($('.owl-carousel').length > 0) {
@@ -93,18 +93,84 @@ items: 1,
                 }
 
         }//responsive end
-
-
-
 });
-        }//owl carousel end
+}//owl carousel end
 
 
 
 
 
 
+jQuery.validator.addMethod("ContainsAtLeastOneDigit", function (value) {
+return /^[a-z]+[0-9]/i.test(value);
+});
+        $(function () {
+        $(".contact-form").validate({
+        highlight: function (element) {
+        $(element).closest('.form-group').addClass("has-danger");
+                $(element).addClass("form-control-danger");
+        },
+                unhighlight: function (element) {
+                $(element).closest('.form-group').removeClass('has-danger').addClass('has-success');
+                        $(element).removeClass('form-control-danger').addClass('form-control-success');
+                },
+                rules: {
+                name: {
+                required: true,
+                        rangelength: [2, 20]
+                },
+                        email: {
+                        required: true,
+                                email: true
+                        },
+                        phone: {
+                        required: true,
+                                number: true
+                        },
+                        message: {
+                        required: true,
+                                maxlength: 255
+                        }
 
-}); // DOCUMENT READY END
+
+
+
+                },
+                messages: {
+                name: {
+                required: 'Please enter your name',
+                        rangelength: '2 - 20 letters'
+
+                },
+                        email: {
+                        required: 'Please enter your email adress',
+                                email: 'Not a valid email adress'
+                        },
+                        phone: {
+                        required: 'Please enter your phone-nr',
+                                number: 'number'
+                        },
+                        message: {
+                        required: 'Say hello',
+                                maxlength: '255 letters limit'
+                        }
+
+                },
+                errorElement: 'p',
+                errorPlacement: function (error, element) {
+                error.appendTo($(element).closest('.form-group').find('.error-msg'));
+                }
+
+        });
+        });
+
+        //Form Validation
+
+
+
+
+
+
+        }); // DOCUMENT READY END
 
 
