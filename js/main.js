bfcc39baa041a26ab8c8e9ae19cf3c57cf91b588
Dejan <Dejan@DESKTOP-F78QALM>
2019-04-18 18:11:58 +0200

    portfolio section complete

--- js/main.js
+++ js/main.js
@@ -60,6 +60,23 @@ $(document).ready(function () {
 
 
 
+//LAXXX ANIMATION PLUGIN INITIALIZATION
+
+    window.onload = function () {
+        lax.setup({
+            breakpoints: {small: 0, large: 992}
+        }); // init
+
+        const updateLax = () => {
+            lax.update(window.scrollY);
+            window.requestAnimationFrame(updateLax);
+        };
+
+        window.requestAnimationFrame(updateLax);
+    };
+
+
+    // LAXXX ANIMATION BREAKPOINT
 
 
 
