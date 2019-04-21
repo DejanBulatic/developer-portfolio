dac3a8328a9cb34c334e3d6ed36935787e28132a
Dejan <Dejan@DESKTOP-F78QALM>
2019-04-21 16:11:33 +0200

    footer complete

--- js/main.js
+++ js/main.js
@@ -1,87 +1,161 @@
 $(document).ready(function () {
 
+
+
+    // SMOOTH SCROLL
+
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 800);
+    });
+
+
+
+// SMALL HEADER ANIMATION ON SCROLL
+
+    function animateHeader() {
+        var scrollTop = $(window).scrollTop();
+        if (scrollTop > 10 && !$('.mobile-nav').hasClass('mobile-nav-show')) {
+            $('.small-header').addClass('small-header-active');
+        } else {
+            $('.small-header').removeClass('small-header-active');
+        }
+    }
+
+    animateHeader();
+
+    $(window).scroll(function () {
+        animateHeader();
+        checkOffset();
+    });
+
+
+
+
+// BURGER BUTTON ANIMATION  
+
+    $('.burger').click(function () {
+
+
+        $(this).toggleClass('active burger-move-right');
+        $('.mobile-nav').toggleClass('mobile-nav-show');
+        $('.body-overlay').toggleClass('body-overlay-show');
+
+
+        if (!$('.mobile-nav').hasClass('mobile-nav-show')) {
+            $('.small-header').addClass('small-header-active');
+        }
+        if ($('.mobile-nav').hasClass('mobile-nav-show')) {
+            $('.small-header').removeClass('small-header-active');
+        }
+
+
+//        
+
+    });// BURGER BUTTON ANIMATION END
+
+
+
+
+
+
 // SIDE BAR TOGGLE ON DESCTOP
 
-$('.side-bar-toggle').click(function () {
-$(this).toggleClass('side-bar-toggle-closed');
+    $('.side-bar-toggle').click(function () {
+        $(this).toggleClass('side-bar-toggle-closed');
         $('.side-bar').toggleClass('side-bar-hide');
         $('main').toggleClass('main-full-width');
         //SMALL ARROW DIRECTION
         if ($('.side-bar-hide').length > 0) {
-$('.side-bar-toggle').removeClass('fa-angle-left');
-        $('.side-bar-toggle').addClass('fa-angle-right');
-        $('.side-bar-toggle').removeClass('toggle-border-left');
-        $('.side-bar-toggle').addClass('toggle-border-right');
-} else {
-$('.side-bar-toggle').removeClass('fa-angle-right');
-        $('.side-bar-toggle').addClass('fa-angle-left');
-        $('.side-bar-toggle').removeClass('toggle-border-right');
-        $('.side-bar-toggle').addClass('toggle-border-left');
-}
-});
-        // BURGER BUTTON ANIMATION  
+            $('.side-bar-toggle').removeClass('fa-angle-left toggle-border-left');
+            $('.side-bar-toggle').addClass('fa-angle-right toggle-border-right');
+        } else {
+            $('.side-bar-toggle').removeClass('fa-angle-right toggle-border-right');
+            $('.side-bar-toggle').addClass('fa-angle-left toggle-border-left');
 
-        $('.burger').click(function () {
-$(this).toggleClass('active burger-move-right');
-        $('.mobile-nav').toggleClass('mobile-nav-show');
-        $('.body-overlay').toggleClass('body-overlay-show');
-});
-        //  OWL BRANDS SLIDER
 
-        if ($('.owl-carousel').length > 0) {
+        }
+    });
 
-$('.brands-slider').owlCarousel({
-loop: true,
-        dots: false,
-        center: true,
+
+
+
+    window.onload = function () {
+        lax.setup({
+            breakpoints: {small: 0, large: 992}
+        }); // init
+
+        const updateLax = () => {
+            lax.update(window.scrollY);
+            window.requestAnimationFrame(updateLax);
+        };
+        window.requestAnimationFrame(updateLax);
+    };
+
+
+
+
+
+
+    //  OWL BRANDS SLIDER
+
+    if ($('.owl-carousel').length > 0) {
+
+        $('.brands-slider').owlCarousel({
+            loop: true,
+            dots: false,
+            center: true,
 //            autoplay: true,
-        responsive: {
-        0: {
-        items: 1
-        },
+            responsive: {
+                0: {
+                    items: 1
+                },
                 500: {
-                items: 2
+                    items: 2
                 },
                 991: {
-                items: 4
+                    items: 4
                 },
                 1200: {
-                items: 5
+                    items: 5
                 }
 
-        }//responsive end
-});
-} // BRANDS SLIDER END
+            }//responsive end
+        });
+    } // BRANDS SLIDER END
 
 
 
 //LAXXX ANIMATION PLUGIN INITIALIZATION
 
-window.onload = function () {
-lax.setup({
-breakpoints: {small: 0, large: 992}
-}); // init
+    window.onload = function () {
+        lax.setup({
+            breakpoints: {small: 0, large: 992}
+        }); // init
 
         const updateLax = () => {
-lax.update(window.scrollY);
+            lax.update(window.scrollY);
+            window.requestAnimationFrame(updateLax);
+        };
         window.requestAnimationFrame(updateLax);
-};
-        window.requestAnimationFrame(updateLax);
-};
+    };
 // OWL TESTIMONIAL SLIDER
 
-        if ($('.owl-carousel').length > 0) {
+    if ($('.owl-carousel').length > 0) {
 
-$('.testimonial-slider').owlCarousel({
-items: 1,
-        loop: true,
-        margin: 20,
+        $('.testimonial-slider').owlCarousel({
+            items: 1,
+            loop: true,
+            margin: 20,
 //            autoplay: true,
-        navText: ['<span class="fa fa-long-arrow-left"></span>', '<span class="fa fa-long-arrow-right"></span>'],
-        responsive: {
-        0: {
+            navText: ['<span class="fa fa-long-arrow-left"></span>', '<span class="fa fa-long-arrow-right"></span>'],
+            responsive: {
+                0: {
 
-        },
+                },
                 500: {
 
                 },
@@ -89,88 +163,99 @@ items: 1,
 
                 },
                 1200: {
-                nav: true
+                    nav: true
                 }
 
-        }//responsive end
-});
-}//owl carousel end
+            }//responsive end
+        });
+    }//owl carousel end
 
 
 
 
 
 
-jQuery.validator.addMethod("ContainsAtLeastOneDigit", function (value) {
-return /^[a-z]+[0-9]/i.test(value);
-});
-        $(function () {
+    jQuery.validator.addMethod("ContainsAtLeastOneDigit", function (value) {
+        return /^[a-z]+[0-9]/i.test(value);
+    });
+    $(function () {
         $(".contact-form").validate({
-        highlight: function (element) {
-        $(element).closest('.form-group').addClass("has-danger");
+            highlight: function (element) {
+                $(element).closest('.form-group').addClass("has-danger");
                 $(element).addClass("form-control-danger");
-        },
-                unhighlight: function (element) {
+            },
+            unhighlight: function (element) {
                 $(element).closest('.form-group').removeClass('has-danger').addClass('has-success');
-                        $(element).removeClass('form-control-danger').addClass('form-control-success');
-                },
-                rules: {
+                $(element).removeClass('form-control-danger').addClass('form-control-success');
+            },
+            rules: {
                 name: {
-                required: true,
-                        rangelength: [2, 20]
+                    required: true,
+                    rangelength: [2, 20]
                 },
-                        email: {
-                        required: true,
-                                email: true
-                        },
-                        phone: {
-                        required: true,
-                                number: true
-                        },
-                        message: {
-                        required: true,
-                                maxlength: 255
-                        }
-
-
-
-
+                email: {
+                    required: true,
+                    email: true
                 },
-                messages: {
-                name: {
-                required: 'Please enter your name',
-                        rangelength: '2 - 20 letters'
-
+                phone: {
+                    required: true,
+                    number: true
                 },
-                        email: {
-                        required: 'Please enter your email adress',
-                                email: 'Not a valid email adress'
-                        },
-                        phone: {
-                        required: 'Please enter your phone-nr',
-                                number: 'number'
-                        },
-                        message: {
-                        required: 'Say hello',
-                                maxlength: '255 letters limit'
-                        }
-
-                },
-                errorElement: 'p',
-                errorPlacement: function (error, element) {
-                error.appendTo($(element).closest('.form-group').find('.error-msg'));
+                message: {
+                    required: true,
+                    maxlength: 255
                 }
 
+
+
+
+            },
+            messages: {
+                name: {
+                    required: 'Please enter your name',
+                    rangelength: '2 - 20 letters'
+
+                },
+                email: {
+                    required: 'Please enter your email adress',
+                    email: 'Not a valid email adress'
+                },
+                phone: {
+                    required: 'Please enter your phone-nr',
+                    number: 'number'
+                },
+                message: {
+                    required: 'Say hello',
+                    maxlength: '255 letters limit'
+                }
+
+            },
+            errorElement: 'p',
+            errorPlacement: function (error, element) {
+                error.appendTo($(element).closest('.form-group').find('.error-msg'));
+            }
+
         });
-        });
+    });
 
-        //Form Validation
-
-
+    //Form Validation
 
 
 
+// SIDE BAR STOP AT FOOTER
 
-        }); // DOCUMENT READY END
+    function checkOffset() {
+        if ($('.side-bar').offset().top + $('.side-bar').height()
+                >= $('footer').offset().top - 0)
+            $('.side-bar').css('position', 'absolute');
+        if ($(document).scrollTop() + window.innerHeight < $('footer').offset().top)
+            $('.side-bar').css('position', 'fixed'); // restore when you scroll up
+    }
+
+
+
+
+
+}); // DOCUMENT READY END
 
 
